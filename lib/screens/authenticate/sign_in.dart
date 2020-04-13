40d509ee031093983ebdf7ec8de05570ee9d2fae
navneeeth <44264834+navneeeth@users.noreply.github.com>
2020-04-14 00:27:52 +0530

    14) Text Input Decoration.

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
 
@@ -47,6 +48,7 @@ String error = '';
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
                 validator: (val) => val.isEmpty ? 'Enter an email.' : null,
                 onChanged: (val) {
                   setState(() => email = val);
@@ -54,6 +56,7 @@ String error = '';
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
                 validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long.' : null,
                 obscureText: true,
                 onChanged: (val) {
