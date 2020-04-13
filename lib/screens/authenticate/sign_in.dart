ae307d234be48a8a4fa1e19c4fca490832bb3655
navneeeth <44264834+navneeeth@users.noreply.github.com>
2020-04-14 00:14:37 +0530

    13) Sign in with Email & Password.

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -14,10 +14,12 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
 
 final AuthService _auth = AuthService();
+final _formKey = GlobalKey<FormState>();
 
 // text field state
 String email ='';
 String password ='';
+String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -40,16 +42,19 @@ String password ='';
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email.' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long.' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -65,9 +70,22 @@ String password ='';
                   ),
                   ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate())
+                  {
+                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    if(result == null) {
+                      setState(() => error = 'Could not sign in with those credentials.');
+                    }
+                  }
                   },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0,
+                ),
               ),
             ],
           ),
